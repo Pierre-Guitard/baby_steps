ff99a6453e1cf896f25db16f15487a5a4cf62576
Romain Nicault <romain@nicault.fr>
2023-11-27 17:46:15 +0100

    all done

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -3,4 +3,5 @@ class Memory < ApplicationRecord
   has_many :key_memory, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :babies, through: :key_memories
+  has_many_attached :photos
 end
