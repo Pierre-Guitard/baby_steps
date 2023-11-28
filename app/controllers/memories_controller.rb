9cbc475c8a8014a66a5559e4bfbd67020ed08990
pierre guitard <pierreguitard78@gmail.com>
2023-11-28 14:18:17 +0100

    wip

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,2 +1,12 @@
 class MemoriesController < ApplicationController
+  before_action :set_memory, only: []
+  
+  def new
+    @memory = Memory.new
+  end
+
+  private
+  def set_memory
+    @memory = Memory.find(params[:id])
+  end
 end
