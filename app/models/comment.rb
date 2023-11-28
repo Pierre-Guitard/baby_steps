5104442cb9d53ccddaf7e1d7e65fe108ba7b1ffa
alix capely <alixcapely@gmail.com>
2023-11-28 12:15:13 +0100

    ajout validations de base, presence et lenght

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :memory
   belongs_to :user
+  validates :content, :memory_id, :user_id, presence: true
+  validates :content, length: { minimum: 10 }
 end
--- app/models/memory.rb
+++ app/models/memory.rb
@@ -4,4 +4,7 @@ class Memory < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :babies, through: :key_memories
   has_many_attached :photos
+  validates :date, :title, :content, :location, :user_id, presence: true
+  validates :title, length: { minimum: 4 }
+  validates :content, length: { minimum: 10 }
 end
