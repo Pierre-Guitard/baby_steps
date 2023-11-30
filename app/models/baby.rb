eabce9721227d6cef3f6819aaa6bb1d982e33abc
Romain Nicault <romain@nicault.fr>
2023-11-30 15:13:20 +0100

    avatars

--- app/models/baby.rb
+++ app/models/baby.rb
@@ -4,4 +4,5 @@ class Baby < ApplicationRecord
   has_many :key_memories, dependent: :destroy
   has_many :memories, through: :key_memories
   validates :first_name, :last_name, :nickname, :birth_date, :sex, presence: true
+  has_one_attached :avatar
 end
