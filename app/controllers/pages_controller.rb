bd1ab7e78a405c3f835122a8b177f33f83d9b785
Romain Nicault <romain@nicault.fr>
2023-12-05 14:51:02 +0100

    it kinda works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,18 +12,21 @@ class PagesController < ApplicationController
     @babies = Baby.all
     @key_memories = KeyMemory.all
     @timeline = []
-    margin = 0
-    @key_memories.where.not(event: "").sort_by { |key_memory| key_memory.memory.date }.each do |key_memory|
-      timeline_event = {
-        days: (key_memory.memory.date - key_memory.baby.birth_date).to_i,
-        event: key_memory.event,
-        memory: key_memory.memory,
-        baby: key_memory.baby,
-        margin: (key_memory.memory.date - key_memory.baby.birth_date).to_i - margin
-      }
-      margin = timeline_event[:margin]
-      @timeline << timeline_event
+    @babies.each do |baby|
+      margin = 0
+      timeline_baby = []
+      baby.key_memories.where.not(event: "").sort_by { |key_memory| key_memory.memory.date }.each do |key_memory|
+        timeline_baby_event = {
+          days: (key_memory.memory.date - key_memory.baby.birth_date).to_i,
+          event: key_memory.event,
+          memory: key_memory.memory,
+          baby: key_memory.baby,
+          margin: (key_memory.memory.date - key_memory.baby.birth_date).to_i - margin
+        }
+        margin = timeline_baby_event[:margin]
+        timeline_baby << timeline_baby_event
+      end
+      @timeline << timeline_baby
     end
-    # @timeline.sort_by! { |hash| hash[:days] }
   end
 end
