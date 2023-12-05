728d5d9ea3ebb5710d35380a8c68304c5db009f8
Romain Nicault <romain@nicault.fr>
2023-12-05 16:46:22 +0100

    omg

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,21 +12,22 @@ class PagesController < ApplicationController
     @babies = Baby.all
     @key_memories = KeyMemory.all
     @timeline = []
-    @babies.each do |baby|
-      margin = 0
-      timeline_baby = []
-      baby.key_memories.where.not(event: "").sort_by { |key_memory| key_memory.memory.date }.each do |key_memory|
-        timeline_baby_event = {
-          days: (key_memory.memory.date - key_memory.baby.birth_date).to_i,
-          event: key_memory.event,
-          memory: key_memory.memory,
-          baby: key_memory.baby,
-          margin: (key_memory.memory.date - key_memory.baby.birth_date).to_i - margin
-        }
-        margin = timeline_baby_event[:margin]
-        timeline_baby << timeline_baby_event
-      end
-      @timeline << timeline_baby
+    @key_memories.where.not(event: "").each do |key_memory|
+      timeline_event = {
+        days: (key_memory.memory.date - key_memory.baby.birth_date).to_i,
+        event: key_memory.event,
+        memory: key_memory.memory,
+        baby: key_memory.baby,
+        margin: 0
+      }
+      @timeline << timeline_event
+    end
+
+    @timeline.sort_by! { |event| event[:days] }
+    margin = 0
+    @timeline.each do |timeline_event|
+      timeline_event[:margin] = timeline_event[:days] - margin
+      margin = timeline_event[:margin]
     end
   end
 end
