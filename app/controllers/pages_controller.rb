e1a51329213721c3f50e6077a5beb2553f72e692
Romain Nicault <romain@nicault.fr>
2023-12-05 14:36:49 +0100

    step stone

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,15 +12,18 @@ class PagesController < ApplicationController
     @babies = Baby.all
     @key_memories = KeyMemory.all
     @timeline = []
-      @key_memories.where.not(event: "").each do |key_memory|
+    margin = 0
+    @key_memories.where.not(event: "").sort_by { |key_memory| key_memory.memory.date }.each do |key_memory|
       timeline_event = {
-          days: (key_memory.memory.date - key_memory.baby.birth_date).to_i,
-          event: key_memory.event,
-          memory: key_memory.memory,
-          baby: key_memory.baby
-        }
-        @timeline << timeline_event
+        days: (key_memory.memory.date - key_memory.baby.birth_date).to_i,
+        event: key_memory.event,
+        memory: key_memory.memory,
+        baby: key_memory.baby,
+        margin: (key_memory.memory.date - key_memory.baby.birth_date).to_i - margin
+      }
+      margin = timeline_event[:margin]
+      @timeline << timeline_event
     end
-    @timeline.sort_by! { |hash| hash[:days] }
+    # @timeline.sort_by! { |hash| hash[:days] }
   end
 end
