aec4d8e90dade48dfb3b601670cc92f8ea080038
Romain Nicault <romain@nicault.fr>
2023-12-06 17:44:59 +0100

    longer_timeline

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,7 @@ class PagesController < ApplicationController
     @memories = Memory.order(date: :desc)
     @babies = Baby.all
     @key_memories = KeyMemory.all
-    
+
   end
 
   def timeline
@@ -19,20 +19,11 @@ class PagesController < ApplicationController
         event: key_memory.event,
         memory: key_memory.memory,
         baby: key_memory.baby,
-        margin: 0
+        margin: ((key_memory.memory.date - key_memory.baby.birth_date).to_i * 2)
       }
       @timeline << timeline_event
     end
 
     @timeline.sort_by! { |event| event[:days] }
-    margin = 0
-    @timeline.each_with_index do |timeline_event, index|
-      if index.zero?
-        timeline_event[:margin] = timeline_event[:days] - margin
-      else
-        timeline_event[:margin] = timeline_event[:days] - margin - 50
-      end
-      margin = timeline_event[:margin]
-    end
   end
 end
