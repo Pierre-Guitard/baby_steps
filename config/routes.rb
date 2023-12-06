7660e6e27fdd38e23533a6aef24d23004bb7f009
pierre guitard <pierreguitard78@gmail.com>
2023-12-06 10:38:23 +0100

    sideNavBar

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :memories, only: [:new, :create] do
     resources :key_memories
   end
-  get "timeline", to: "pages#timeline"
+  get "timeline", to: "pages#timeline", as: :timeline
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
